e846ff0d98d806a090b1e54a9fa4b66ae5515178
Michael Schwarz <michael.schwarz91@gmail.com>
2025-04-24 23:38:27 +0200

    remove old code

--- module/pteditor.c
+++ module/pteditor.c
@@ -17,9 +17,9 @@
 #include <linux/mmap_lock.h>
 #endif
 
-#ifdef CONFIG_PAGE_TABLE_ISOLATION
-pgd_t __attribute__((weak)) __pti_set_user_pgtbl(pgd_t *pgdp, pgd_t pgd);
-#endif
+//#ifdef CONFIG_PAGE_TABLE_ISOLATION
+//pgd_t __attribute__((weak)) __pti_set_user_pgtbl(pgd_t *pgdp, pgd_t pgd);
+//#endif
 
 static int real_page_size = 4096, real_page_shift = 12;
 
